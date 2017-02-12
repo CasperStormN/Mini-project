9672b231b403f2d686f9de7925645cc72f0d12fd
Casper Storm <casper.storm@hotmail.com>
2017-02-12 18:16:35 +0100

    Details page done

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -13,5 +13,17 @@ jQuery( document ).ready(function() {
   		//'auto': false,
   		'slideWrapper': '<div class="sy-slides-wrap" />'
   	})
+
+  	jQuery('.nav-tabs li').click(function() {  		
+  		jQuery('.nav-tabs li').removeClass('active');
+  		jQuery(this).addClass('active');
+  	});
+
+  	jQuery('.gallery .img-container').click(function() {  		
+  		jQuery('.gallery .img-container').removeClass('active');
+  		jQuery(this).addClass('active');
+  		//var img = jQuery(this).find('img').attr('src');
+  		//jQuery('.preview').find('img').attr('src', img);
+  	});
 });
 
--- public/details.php
+++ public/details.php
@@ -1,19 +1,19 @@
 <?php require_once('header.php'); ?>
 
 <div id="details">
-	<div class="container">
+	<div class="container basic-info">
 		<div class="col-xs-12 col-md-6 img-preview">
-			<div class="img-container">
+			<div class="img-container preview">
 				<img src="assets/img/shoe-big.jpg" alt="">
 			</div>
 			<div class="gallery">
-				<div class="img-container col-xs-12 col-md-4">
+				<div class="img-container">
 					<img src="assets/img/shoe-side.jpg" alt="">
 				</div>
-				<div class="img-container col-xs-12 col-md-4">
+				<div class="img-container middle active">
 					<img src="assets/img/shoe-atop.jpg" alt="">
 				</div>
-				<div class="img-container col-xs-12 col-md-4">
+				<div class="img-container">
 					<img src="assets/img/shoe-front.jpg" alt="">
 				</div>
 			</div>
@@ -55,12 +55,12 @@
 		<ul class="nav nav-tabs">
 			<div class="container">
 				<li class="active"><div class="active-border"><a>Description</a></div></li>
-				<li><a>Shipping</a></li>
-				<li><a>Sizing</a></li>
+				<li><div class="active-border"><a>Shipping</a></div></li>
+				<li><div class="active-border"><a>Sizing</a></div></li>
 			</div>
 		</ul>
 		<div class="container">
-			<div class="tabs-text description-text">
+			<div class="tabs-text description">
 				<div class="col-xs-12 col-md-6">
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit, elit vitae porttitor facilisis, urna nunc sollicitudin nibh, eu sodales metus nisi a orci. Cras posuere, mauris sit amet congue commodo, ex mauris auctor massa, at tincidunt massa quam eget mauris. Suspendisse quis dignissim lacus.</p>
 					<br>
@@ -69,6 +69,26 @@
 				<div class="col-xs-12 col-md-6">
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit, elit vitae porttitor facilisis, urna nunc sollicitudin nibh, eu sodales metus nisi a orci. Cras posuere, mauris sit amet congue commodo, ex mauris auctor massa, at tincidunt massa quam eget mauris. Suspendisse quis dignissim lacus.</p>
 					<br>
+					<p>Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus. Fusce malesuada justo quis mauris scelerisque finibus. Cras porta, tortor non interdum fringilla, lorem erat aliquam justo, ut pulvinar arcu diam in odio. Etiam at nulla commodo, bibendum dolor sed, molestie justo. Integer nulla metus, efficitur eu malesuada placerat, porttitor et sapien.</p>
+				</div>
+			</div>
+			<div class="tabs-text Shipping">
+				<div class="col-xs-12 col-md-6">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit, elit vitae porttitor facilisis, urna nunc sollicitudin nibh, eu sodales metus nisi a orci. Cras posuere, mauris sit amet congue commodo, ex mauris auctor massa, at tincidunt massa quam eget mauris. Suspendisse quis dignissim lacus.</p>
+					<br>
+					<p>Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus. Fusce malesuada justo quis mauris scelerisque finibus. Cras porta, tortor non interdum fringilla, lorem erat aliquam justo, ut pulvinar arcu diam in odio. Etiam at nulla commodo, bibendum dolor sed, molestie justo. Integer nulla metus, efficitur eu malesuada placerat, porttitor et sapien.</p>
+				</div>
+				<div class="col-xs-12 col-md-6">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit, elit vitae porttitor facilisis, urna nunc sollicitudin nibh, eu sodales metus nisi a orci. Cras posuere, mauris sit amet congue commodo, ex mauris auctor massa, at tincidunt massa quam eget mauris. Suspendisse quis dignissim lacus.</p>
+				</div>
+			</div>
+			<div class="tabs-text sizing">
+				<div class="col-xs-12 col-md-6">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit, elit vitae porttitor facilisis, urna nunc sollicitudin nibh, eu sodales metus nisi a orci. Cras posuere, mauris sit amet congue commodo, ex mauris auctor massa, at tincidunt massa quam eget mauris. Suspendisse quis dignissim lacus.</p>
+					<br>
+					<p>Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus. Fusce malesuada justo quis mauris scelerisque finibus. Cras porta, tortor non interdum fringilla, lorem erat aliquam justo, ut pulvinar arcu diam in odio. Etiam at nulla commodo, bibendum dolor sed, molestie justo. Integer nulla metus, efficitur eu malesuada placerat, porttitor et sapien.</p>
+				</div>
+				<div class="col-xs-12 col-md-6">
 					<p>Duis vitae ante non diam porttitor porttitor nec non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac risus cursus lectus fermentum consectetur. In hac habitasse platea dictumst. Fusce tristique rutrum elit id luctus. Fusce malesuada justo quis mauris scelerisque finibus. Cras porta, tortor non interdum fringilla, lorem erat aliquam justo, ut pulvinar arcu diam in odio. Etiam at nulla commodo, bibendum dolor sed, molestie justo. Integer nulla metus, efficitur eu malesuada placerat, porttitor et sapien.</p>
 				</div>
 			</div>
